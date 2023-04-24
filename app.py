f93adc67716ad065c5cc5ce8e45f6705581623dc
Alapakam Dorababu <37065528+dorababu067@users.noreply.github.com>
2023-04-24 11:10:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import logging
 import smtplib, ssl
 from flask import Flask
 from datetime import datetime
 from email.message import EmailMessage
 from flask_apscheduler import APScheduler
+from decouple import config
 
 
 app = Flask(__name__)
@@ -14,7 +16,29 @@ scheduler.start()
 
 @scheduler.task(trigger="interval", id="#send_email", seconds=20)
 def send_mail():
-    print("Mail sent successfully.")
+    port = 465
+    smtp_server = "smtp.gmail.com"
+
+    message = EmailMessage()
+    message["From"] = "Alapakam Dorababu <dorababua67@gmail.com>"
+    message["To"] = "dorababua67@gmail.com"
+    message["Subject"] = "Greetings"
+
+    date_time = datetime.now().strftime("%c")
+    content = f"Hey time date and is: {date_time}"
+    message.set_content(content)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        try:
+            server.login(config("MAIL_USERNAME"), config("MAIL_PASSWORD"))
+            server.send_message(message)
+            logging.info("Mail send sucessfully")
+        except:
+            logging.exception("Authentication Error")
+
+
+send_mail()
 
 
 @app.route("/")
