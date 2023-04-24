46f7140d419b0825832fb6a84e48fb076d2e4f4c
Alapakam Dorababu <37065528+dorababu067@users.noreply.github.com>
2023-04-24 08:35:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,29 +13,6 @@ scheduler.init_app(app)
 scheduler.start()
 
 
-@scheduler.task(trigger="interval", id="#send_email", seconds=20)
-def send_mail():
-    logger.info("Mail sent successfully.")
-    # port = 465
-    # smtp_server = "smtp.gmail.com"
-    # username = "dorababua67@gmail.com"
-    # password = "gylbwsryythnhfko"
-
-    # message = EmailMessage()
-    # message["From"] = "Alapakam Dorababu <dorababua67@gmail.com>"
-    # message["To"] = "dorababua67@gmail.com"
-    # message["Subject"] = "Greetings"
-
-    # date_time = datetime.now().strftime("%c")
-    # content = f"Hey time date and is: {date_time}"
-    # message.set_content(content)
-
-    # context = ssl.create_default_context()
-    # with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    #     server.login(username, password)
-    #     server.send_message(message)
-
-
 @app.route("/")
 def home():
     return {"message": "Welcome to the flask scheduler."}
